f4ca83ca7233646d8d942b2e47a145197f1cd941
Ian Bicking <ianb@colorstudy.com>
2012-07-12 00:55:01 -0500

    fix up generalstorage

--- seeit-services/seeitservices/generalstorage.py
+++ seeit-services/seeitservices/generalstorage.py
@@ -3,6 +3,7 @@ from webob.dec import wsgify
 from webob import Response
 from webob import exc
 from seeitservices.util import JsonFile
+from seeitservices.subber import Subber
 import tempita
 
 
@@ -13,100 +14,37 @@ class GeneralStorage(object):
         if not os.path.exists(dir):
             os.makedirs(dir)
         self.data_fn = os.path.join(dir, 'statuses.json')
+        self.static_app = Subber(
+            'package:seeitservices.generalstorage:./static-generalstorage/')
 
     @wsgify
     def __call__(self, req):
-        if req.path_info == '/describe':
-            return Response(json=self.description(req.application_url))
-        if req.path_info == '/':
-            return self.homepage(req)
-        if req.path_info == '/post':
-            return self.post(req)
-        return exc.HTTPNotFound()
-
-    def description(self, base_url):
-        return dict(
-            name="Simple global storage for lots of data",
-            post=base_url + '/post',
-            types=['status', 'song'],
-            )
+        if req.path_info == '/data':
+            return self.data_app(req)
+        return self.static_app
 
     data = JsonFile('data_fn')
 
-    def post(self, req):
-        if req.method != 'POST':
-            return exc.HTTPMethodNotAllowed(allow='POST')
+    @wsgify
+    def data_app(self, req):
+        if req.method not in ('GET', 'POST', 'DELETE'):
+            return exc.HTTPMethodNotAllowed(allow='GET,POST,DELETE')
         data = self.data
-        body = req.json
-        items = self.explode_items(body, ['status', 'song'])
-        data.extend(items)
-        self.data = data
-        result = self.success_template.substitute(url=req.application_url + '/')
-        return Response(result)
-
-    def explode_items(self, data, keys):
-        result = []
-        for key in keys:
-            if not data.get(key):
-                continue
-            items = data[key]
-            if not isinstance(items, (list, tuple)):
-                items = [items]
-            for item in items:
-                item['type'] = key
-                item['location'] = data['location']
-                result.append(item)
-        return result
-
-    success_template = tempita.HTMLTemplate("""\
-Saved! <a target="_blank" href="{{ url }}">view results</a>""")
-
-    homepage_template = tempita.HTMLTemplate("""\
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Status Updates From Everywhere</title>
-</head>
-<body>
-
-<h1>Status Updates From Everywhere</h1>
-
-{{if not data}}
-There are no items
-{{else}}
-
-<ul>
-{{for item in reversed(data)}}
-<li>
- {{if item['type'] == 'status'}}
-  {{if item.get('profilePic') }}
-    <img src="{{item['profilePic']}}">
-  {{endif}}
-  {{if item.get('profileLink') }}
-    <a href="{{item['profileLink']}}">user</a>
-  {{endif}}
-  at {{item.get('date')}}:
-  {{item.get('body') | html}}
-
- {{elif item['type'] == 'song'}}
-  {{item.get('title')}} by {{item.get('artist')}}
-  {{if item.get('album')}}
-    in {{item['album']}}
-  {{endif}}
-
- {{else}}
-  Unknown item:
-  <pre>{{item}}</pre>
- {{endif}}
-</li>
-{{endfor}}
-</ul>
-
-{{endif}}
-
-""")
-
-    def homepage(self, req):
-        return Response(self.homepage_template.substitute(
-                data=self.data,
-                req=req))
+        data.setdefault('items', [])
+        if req.method == 'POST':
+            body = req.json
+            data['items'].extend(body['items'])
+            self.data = data
+            return Response(json=dict(status="okay"))
+        elif req.method == 'DELETE':
+            id = req.GET['id']
+            for item in list(data['items']):
+                if item.get('id') == id:
+                    data['items'].remove(item)
+                    break
+            else:
+                return Response(status=400, content_type='text/plain',
+                                body='No item found with id %r' % id)
+            return Response(status=200, body='')
+        else:
+            return Response(json=data)
---
+++ seeit-services/seeitservices/static-generalstorage/generalstorage.js
@@ -0,0 +1,159 @@
+function onauthready() {
+  Auth.watch({
+    onlogin: function (email) {
+      console.log('logged in', email, !!dataToSend);
+      $('#login').text(email);
+      if (dataToSend) {
+        send(dataToSend);
+      }
+      startFetch();
+    },
+    onlogout: function () {
+      console.log('logged out');
+      $('#login').text('login');
+    }
+  });
+}
+if (typeof Auth != "undefined") {
+  onauthready();
+}
+
+$(function () {
+  $('#login').click(function () {
+    if (Auth.email) {
+      Auth.logout();
+    } else {
+      Auth.request();
+    }
+  });
+
+  $('.remove').live('click', function () {
+    var el = $(this);
+    var id = el.attr('data-id');
+    $.ajax({
+      url: './data?id=' + encodeURIComponent(id),
+      type: 'DELETE',
+      success: function (resp) {
+        el.remove();
+      },
+      error: function (req, status, thrown) {
+        console.log('Error in remove:', status);
+      }
+    });
+    return false;
+  });
+});
+
+function startFetch() {
+  var genTmpl = $('script#general-item').text();
+  $.ajax({
+    url: './data',
+    type: 'GET',
+    dataType: 'json',
+    success: function (resp) {
+      console.log('starting render');
+      for (var i=0; i<resp.items.length; i++) {
+        var item = resp.items[i];
+        if (presentObjects.has(item)) {
+          continue;
+        }
+        presentObjects.add(item);
+        var tmpl = $('script#' + item.type).text();
+        console.log('got tmpl', tmpl);
+        if (! tmpl) {
+          tmpl = $('script#unknown').text();
+        }
+        console.warn('SOURCE', _.template(tmpl).source);
+        var result = _.template(tmpl, {item: item});
+        console.log('rendered', item, 'result', result, tmpl);
+        result = _.template(genTmpl, {item: item, detail: result});
+        var el = $(result);
+        $('#items').append(el);
+      }
+    },
+    error: function (xhr, status, thrown) {
+      $('#items').append($('<li></li>').text('Error: ' + status + ' thrown: ' + thrown));
+    }
+  });
+}
+
+var dataToSend = null;
+
+function ObjectSet(ignoreAttrs) {
+  if (_.isArray(ignoreAttrs)) {
+    var v = {};
+    _.each(ignoreAttrs, function (i) {v[i] = true;});
+    ignoreAttrs = v;
+  }
+  this._ignoreAttrs = ignoreAttrs || {};
+  this._objs = {};
+}
+
+ObjectSet.prototype = {
+  add: function (obj) {
+    var key = this.makeKey(obj);
+    this._objs[key] = true;
+  },
+  remove: function (obj) {
+    var key = this.makeKey(obj);
+    delete this._objs[key];
+  },
+  has: function (obj) {
+    var key = this.makeKey(obj);
+    return this._objs[key];
+  },
+  makeKey: function (obj) {
+    if (typeof obj == "function") {
+      return '';
+    }
+    if (typeof obj != "object" || obj === null) {
+      return obj + '';
+    }
+    if (_.isArray(obj)) {
+      var s = '[';
+      for (var i=0; i<obj.length; i++) {
+        s += this.makeKey(obj[i]);
+      }
+      return s + ']';
+    }
+    var s = '{';
+    var keys = _.keys(obj).sort();
+    for (var i=0; i<keys.length; i++) {
+      if (this._ignoreAttrs[keys[i]]) {
+        continue;
+      }
+      s += keys[i] + this.makeKey(obj[keys[i]]);
+    }
+    return s + '}';
+  }
+};
+
+var presentObjects = new ObjectSet(['captureDate', 'id']);
+
+
+function send(data) {
+  console.log('sending data', data);
+  if (! Auth.email) {
+    console.log('Delaying for login');
+    dataToSend = data;
+    Auth.request();
+    return;
+  }
+  dataToSend = null;
+  for (var i=0; i<data.data.length; i++) {
+    var item = data.data[i];
+    item.location = data.location;
+    item.captureDate = data.date;
+    item.type = data.type;
+    item.id = jQuery.makeUUID();
+  }
+  $.ajax({
+    url: './data',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({items: data.data}),
+    success: function (resp) {
+      startFetch();
+    }
+  });
+}
